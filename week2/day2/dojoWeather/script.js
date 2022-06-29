3dfdc398170aa083edb4bfcdcffae95ccbe43975
Michael Natter <natter.michael@gmail.com>
2022-06-29 16:58:59 -0600

    commit

---
+++ week2/day2/dojoWeather/script.js
@@ -0,0 +1,15 @@
+
+var cookiePopUp = document.querySelector(".bottomCookie");
+
+function hide(element) {
+    element.remove();
+}
+
+console.log("hello");
+function changeTemp() {
+    var temp = document.querySelector("#temperature");
+    var fTemp
+        if (temp.value == "farenheit") {
+            console.log(temp.value);
+        }
+}
